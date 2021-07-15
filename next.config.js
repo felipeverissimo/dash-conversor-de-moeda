1a4faf8cd3efd3e31e5467b1f8ca7ae4b0019e17
Felipe Silva <felipe.silva@ultreia.com.br>
2021-07-14 21:12:59 -0300

    add configs for styled images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withImages = require("next-images");
+const path = require('path');
+
+module.exports = withImages({
+  esModule: true,
+  fileExtensions: ["jpg", "jpeg", "png", "gif"],
+  inlineImageLimit: false,
+  images:{
+    domains: ['/public/']
+  },
+  target: "serverless"
+});
