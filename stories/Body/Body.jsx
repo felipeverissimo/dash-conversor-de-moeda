ad15008ce2e92879506c72d3275b57e1d47c89f3
Felipe Silva <felipe.silva@ultreia.com.br>
2021-07-14 21:15:58 -0300

    adjusted body component

--- stories/Body/Body.jsx
+++ stories/Body/Body.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types';
 import {ConversionBody} from './body-styling'
-
-
 
 export  const Body =  ({children}) =>{
     return (
 
-        <ConversionBody>
+        <ConversionBody >
             <div className='container'>
               {children}  
             </div>
         </ConversionBody>
   )
 }
-
-Body.propTypes = {
-   /**
-   Checks if the button should be disabled
-  */
-};
